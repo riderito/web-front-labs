42bc0e2fbd6f5f00abb1ecd90110ad443ec9cf49
riderito <lyosha_stepuk@mail.ru>
2024-05-24 12:07:49 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -6,5 +6,11 @@ function showTime() {
     let currentSecond = today.getSeconds();
     document.getElementById('seconds').innerHTML =
         currentSecond < 10 ? '0' + currentSecond : currentSecond;
+    
+    let sec = 180+(currentSecond* 6);
+    let arrow = document.getElementById('arrow');
+    arrow.style.transform = `rotate(${sec}deg) translate(0, 50px)`;
+    arrow.style.transition = 'transform 1s';
+
 }
 setInterval(showTime, 1000);
